ad7e209b9bd67e3a39c69feb7848f427e507553a
renzbagaporo <renz.bagaporo@gmail.com>
2024-12-28 09:38:39 +0900

    Temporarily use similar hal

--- firmware/busdancer-hal/src/lib.rs
+++ firmware/busdancer-hal/src/lib.rs
@@ -3,11 +3,18 @@
 #![allow(clippy::must_use_candidate)]
 
 // modules
+pub mod gpio;
+pub mod serial;
+pub mod timer;
 #[cfg(feature = "usb")]
 pub mod usb;
 
 // re-export dependencies
 #[cfg(feature = "usb")]
 pub use smolusb;
+
+pub use embedded_hal as hal;
+pub use embedded_hal_0 as hal_0;
+pub use embedded_hal_nb as hal_nb;
 
 pub use nb;
--- firmware/busdancer-pac/src/lib.rs
+++ firmware/busdancer-pac/src/lib.rs
@@ -2,6 +2,14 @@
 
 pub mod csr;
 
+pub mod clock {
+    const SYSTEM_CLOCK_FREQUENCY: u32 = 500_000_000;
+
+    #[must_use]
+    pub const fn sysclk() -> u32 {
+        SYSTEM_CLOCK_FREQUENCY
+    }
+}
 
 mod generated;
 pub use generated::*;
